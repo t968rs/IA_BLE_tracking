f2fc18a7cd38ac89040eabc50ecdfa093fce686c
t968rs <kevtg1212@gmail.com>
2024-08-13 23:52:31 -0600

    2nd

--- py/get_esri_spatial_here.py
+++ py/get_esri_spatial_here.py
@@ -4,6 +4,15 @@ import json
 import pandas as pd
 
 
+def process_date(x):
+    from datetime import datetime
+    try:
+        # Try to parse the date to check if it's a valid datetime
+        dt = datetime.fromisoformat(str(x))
+        return dt.strftime('%Y/%m/%d')
+    except ValueError:
+        return x
+
 def read_json_to_dict(file: str) -> dict:
     with open(file, 'r') as f:
         return json.load(f)
@@ -22,13 +31,64 @@ def add_numbered_primary_key(gdf, col_name):
     return gdf
 
 
-def gdf_to_geojson(gdf, out_loc, filename=None):
+column_mapping = {"Iowa_BLE_Tracking": {"huc8": "HUC8", "which_grid": "which_grid", "name": "Name", "Name HUC8": None,
+                                        "has_AECOM": "Has AECOM Tie"}}
 
+column_orders = {"Iowa_BLE_Tracking": {"first": ['huc8', 'which_grid', "name", "PBL_Assign", "Phase_1_Su"],
+                                       "last": ['geometry']}, }
+
+
+def remove_time_from_date_columns(gdf):
+
+    """
+    Remove the time part from date columns in a GeoDataFrame.
+
+    Parameters:
+    gdf (GeoDataFrame): The GeoDataFrame whose date columns need to be processed.
+    date_columns (list): A list of column names that contain date strings with a "T".
+
+    Returns:
+    GeoDataFrame: The GeoDataFrame with modified date columns.
+    """
+    # Convert Timestamp objects to strings
+    for col in gdf.columns:
+        if pd.api.types.is_datetime64_any_dtype(gdf[col]):
+            print(f"Converting {col} to string")
+            gdf[col] = gdf[col].astype(str)
+            gdf[col] = gdf[col].apply(process_date)
+
+    return gdf
+
+
+def reorder_gdf_columns(gdf, first_columns, last_columns=None):
+    if last_columns is None:
+        last_columns = []
+
+    # Columns that are not in first_columns or last_columns
+    middle_columns = [col for col in gdf.columns if col not in first_columns and col not in last_columns]
+
+    # New column order
+    new_column_order = first_columns + middle_columns + last_columns
+
+    # Reorder the columns in the GeoDataFrame
+    gdf = gdf[new_column_order]
+
+    return gdf
+
+
+def gdf_to_geojson(gdf, out_loc, filename=None):
     driver = "GeoJSON"
     outpath = out_loc + f"{filename}.geojson"
     os.makedirs(out_loc, exist_ok=True)
     gdf.to_file(filename=outpath, driver=driver)
+    df = pd.DataFrame(gdf.drop(columns='geometry'))
+
+    outpath_table = out_loc + filename + ".json"
+    dicted = df.to_dict(orient='index')
+    with open(outpath_table, 'w') as f:
+        json.dump(dicted, f)
     print(f"Saved {filename} to {outpath}")
+    print(f"Columns: {gdf.columns.to_list()}")
 
 
 class WriteNewGeoJSON:
@@ -64,7 +124,7 @@ class WriteNewGeoJSON:
                                 name = filename.split(".")[0]
                             else:
                                 name = filename
-                            print(f'   Name: {name}, \n   Filename: {filename}')
+                            print(f'   Name: {name} \n   Filename: {filename}\n')
                             self.esri_files[name] = entry.path
 
         for fname, path, in self.esri_files.items():
@@ -74,25 +134,34 @@ class WriteNewGeoJSON:
             else:
                 gdf = gpd.read_file(path)
 
-            self.crs_dict[name] = gdf.crs
+            self.crs_dict[fname] = gdf.crs
             gdf = gdf.explode(ignore_index=True)
             gdf = add_numbered_primary_key(gdf, 'loc_id')
+            if fname in column_orders:
+                gdf = reorder_gdf_columns(gdf, column_orders[fname]["first"], column_orders[fname]["last"])
+
+            # Fix* times
+            time_cs = [c for c in gdf.columns if gdf[c].astype(str).str.contains('T').any()]
+            print(f"Time columns: {time_cs}")
+            gdf = remove_time_from_date_columns(gdf)
             gdf = gdf.to_crs(epsg=4326)
-            self.c_lists[name] = [c for c in gdf.columns.to_list()]
-            print(f'   Input Columns: {self.c_lists[name]}, \n   CRS: {self.crs_dict[name]}')
-            self.gdf_dict[name] = gdf
+            self.c_lists[fname] = [c for c in gdf.columns.to_list()]
+            print(f'   {fname} Input Columns: {self.c_lists[fname]}, \n   CRS: {self.crs_dict[fname]}')
+            self.gdf_dict[fname] = gdf
 
     def export_geojsons(self, cname_to_summarize=None):
+        print(f'{self.gdf_dict.keys()}')
         for name, gdf in self.gdf_dict.items():
+            print(f"Found {name}")
             if cname_to_summarize is not None and cname_to_summarize in gdf.columns:
                 unique_names = gdf[cname_to_summarize].unique()
                 print(f"Unique {cname_to_summarize} values: {[u for u in unique_names if u]}")
                 print(f'   Plus, {None if None in unique_names else "No None"}  values')
                 gdf_to_geojson(gdf, self.output_folder, name)
+            else:
+                print(f"{cname_to_summarize} not in {name} columns")
 
 
 cname = "which_grid"
-WriteNewGeoJSON().export_geojsons(cname)
-
-
-
+to_gdf = WriteNewGeoJSON()
+to_gdf.export_geojsons(cname)
--- src/mapInteractions.js
+++ src/mapInteractions.js
@@ -1,5 +1,7 @@
 // Function to create popup content
 
+let whichGrid = {'0, 1, 2': "No Grids",
+    '1, 2': "DRAFT, Add'l", '2': "Add'l", "All on MM": "All on MM"};
 
 let unwanted = ["OID_", "OID", "OBJECTID", "info_src", "cur_miss", "route_id", "GlobalID", "Shape__Area",
     "Shape__Length", "Shape__Are", "Shape__Len", "Shape__Area_", "States", "GlobalID"];
@@ -12,57 +14,101 @@ function containsUnwantedSubstring(property) {
     return unwantedSubstrings.some(substring => property.toLowerCase().includes(substring));
 }
 
+// Function to create popup content with table formatting
 export async function areaPopupContent(clickedfeature) {
-    // Fetch the alias mapping JSON file
     let popupContent = '<strong><p style="font-size: 14px;">Iowa BLE Area Info</strong><p>';
+    popupContent += '<table class="popup-table"><thead><tr><th></th><th></th></tr></thead><tbody>';
+
     for (let property in clickedfeature.properties) {
-
-        if (!unwanted.includes(property) || !containsUnwantedSubstring(property)) {
-            let value = clickedfeature.properties[property];
-            let displayProperty = property.replace(/_/g, ' ')
-                .replace('  ', ' ').replace("Su", "Submit")
-                .replace("Mapping In", "Ph1 Mapped By");
-            popupContent += `<p><strong>${displayProperty}</strong>: ${value}</p>`
-
+        if (!unwanted.includes(property) && !containsUnwantedSubstring(property)) {
+            let displayValue, displayProperty;
+            if (property === "which_grid") {
+                displayValue = whichGrid[clickedfeature.properties[property]];
+                displayProperty = "Grids TODO"
+            } else {
+                displayValue = clickedfeature.properties[property];
+                displayProperty = property.replace(/_/g, ' ')
+                    .replace('  ', ' ').replace("Su", "Submit")
+                    .replace("Mapping In", "Ph1 Mapped By")
+            }
+            popupContent += `<tr><td><strong>${displayProperty}</strong></td><td>${displayValue}</td></tr>`;
         }
     }
+
+    popupContent += '</tbody></table>';
     return popupContent;
 }
 
-export async function createRouteLinePopupContent(clickedfeature) {
-    // Fetch the alias mapping JSON file
-    let popupContent = '<strong><p style="font-size: 16px;">Route Information </strong><p>';
-    const response = await fetch('./data/routes_columns.json');
-    const columnDictionaries = await response.json();
-    const aliasMapping = columnDictionaries['field_aliases'];
-    console.log("Route Alias Mapping: ", Object.keys(aliasMapping));
+// Function to check if a layer is visible
+function isLayerVisible(map, layerId) {
+    console.log("Layer ID: ", layerId);
+    const visibility = map.getLayoutProperty(layerId, 'visibility');
+    console.log("Visibility: ", visibility);
+    return visibility !== 'none';
+}
 
-    // Generate the popup content
-    for (let property in clickedfeature.properties) {
-        // Use alias if available, otherwise use the original property name
-        const aliasProperty = aliasMapping[property] || property;
-        if (!unwanted.includes(property)) {
-            let value = clickedfeature.properties[property];
+// Function to create legend items
+export async function createLegendItem(color, label, isAlias = false) {
+    const item = document.createElement('div');
+    const key = document.createElement('span');
+    key.className = 'legend-key';
+    key.style.backgroundColor = color;
 
-            // Check if the property name includes 'date' and matches the specific pattern
-            if (property.toLowerCase().includes('date') && typeof value === 'string'
-                && value.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/)) {
-                value = value.substring(0, 10); // Extract the date part
-            } else if (typeof value === "number") {
-                // Check if the value is a float and limit the decimals
-                if (!Number.isInteger(value)) {
-                    value = value.toFixed(1); // Limit to 2 decimal places
+    const value = document.createElement('span');
+    value.innerHTML = label;
+    if (isAlias) {
+        value.style.fontWeight = 'bold';
+        value.style.fontFamily = 'Century Gothic';
+    }
+
+    item.appendChild(key);
+    item.appendChild(value);
+    return item;
+}
+
+// Function to populate the legend
+export async function populateLegend(map, layersToInclude) {
+    const legend = document.getElementById('legend');
+    if (!legend) {
+        console.error('Legend element not found');
+        return;
+    }
+    legend.innerHTML = ''; // Clear existing legend items
+
+    const layers = map.getStyle().layers;
+
+    // Iterate through the specified layers
+    for (const { id, alias } of layersToInclude) {
+        if (!isLayerVisible(map, id)) {
+            continue; // Skip layers that are not visible
+        }
+
+        const layer = layers.find(layer => layer.id === id);
+        if (layer) {
+            const paint = layer.paint;
+            const colorProperty = paint['fill-color'] || paint['line-color'];
+
+            if (colorProperty && Array.isArray(colorProperty) && colorProperty[0] === 'match') {
+                const colorMapping = colorProperty.slice(2, -1);
+
+                // Add alias name once
+                const aliasItem = await createLegendItem('', alias, true);
+                legend.appendChild(aliasItem);
+
+                for (let i = 0; i < colorMapping.length; i += 2) {
+                    const propertyValue = colorMapping[i];
+                    const color = colorMapping[i + 1];
+
+                    // Skip "other" or "*" values
+                    if (propertyValue !== '* other *') {
+                        const displayValue = whichGrid[propertyValue] || propertyValue;
+                        const item = await createLegendItem(color, displayValue);
+                        legend.appendChild(item);
+                    }
                 }
-            } else if (property === "cur_miss") {
-                let shorterText = value.substring(0, 42);
-                popupContent += `<p><strong>${aliasProperty}</strong>: ${shorterText}</p>`;
-                continue; // Skip the default addition for this property
             }
-
-            popupContent += `<p><strong>${aliasProperty}</strong>: ${value}</p>`;
         }
     }
-    return popupContent;
 }
 
 // Function to fit map to the bounds of the specified layer
@@ -144,6 +190,56 @@ export function closePopup() {
     } else {
         console.error('Popup element not found');
     }
+}
+
+// Function to update the legend based on layer visibility
+export function updateLegendOnVisibilityChange(map, layersToInclude) {
+    map.on('data', async (e) => {
+        if (e.dataType === 'source' && e.isSourceLoaded) {
+            await populateLegend(map, layersToInclude);
+        }
+    });
+
+    // Add event listener for layer visibility change
+    map.on('idle', async () => {
+        await populateLegend(map, layersToInclude);
+    });
+}
+
+export function createLayerControls(map, layerGroups) {
+    const controlTable = document.getElementById('layer-controls-table');
+    if (!controlTable) {
+        console.error('Layer controls table not found');
+        return;
+    }
+
+    for (const [group, layers] of Object.entries(layerGroups)) {
+        const groupRow = document.createElement('tr');
+
+        const checkboxCell = document.createElement('td');
+        const groupCheckbox = document.createElement('input');
+        groupCheckbox.type = 'checkbox';
+        groupCheckbox.checked = true;
+        groupCheckbox.addEventListener('change', () => toggleLayerGroup(map, layers, groupCheckbox.checked));
+        checkboxCell.appendChild(groupCheckbox);
+
+        const labelCell = document.createElement('td');
+        const groupLabel = document.createElement('label');
+        groupLabel.textContent = group;
+        labelCell.appendChild(groupLabel);
+
+        groupRow.appendChild(checkboxCell);
+        groupRow.appendChild(labelCell);
+        controlTable.appendChild(groupRow);
+    }
+}
+
+function toggleLayerGroup(map, layers, visibility) {
+    layers.forEach(layerId => {
+        if (map.getLayer(layerId)) {
+            map.setLayoutProperty(layerId, 'visibility', visibility ? 'visible' : 'none');
+        }
+    });
 }
 
 
