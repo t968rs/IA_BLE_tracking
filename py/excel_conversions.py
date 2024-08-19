060d7e7df7c4ce9e3c9d1978b81453a7e56c2395
t968rs <kevtg1212@gmail.com>
2024-08-19 14:53:38 -0600

    added points

--- py/excel_conversions.py
+++ py/excel_conversions.py
@@ -29,20 +29,31 @@ def write_excel_to_df(file: str, sheet_name: str) -> pd.DataFrame:
 def update_geojson_with_dataframe(geojson_path, excel_path, common_column):
     # Read the GeoJSON file into a GeoDataFrame
     gdf = gpd.read_file(geojson_path)
+    gdf[common_column] = gdf[common_column].astype("int8")
 
     # Read the Excel file into a DataFrame
-    df = pd.read_excel(excel_path)
+    df = pd.read_excel(excel_path).drop(columns=['Unnamed: 0'])
+    print(df.columns)
+    df[common_column] = df[common_column].astype("int8")
 
     # Merge the GeoDataFrame and DataFrame on the common column
-    merged_gdf = gdf.merge(df, on=common_column, how='left', suffixes=('', '_updated'))
+    merged_gdf = gdf.join(df, on=common_column, how='inner', rsuffix='_updated')
+    print(f'Merged GDF: {merged_gdf.columns}')
 
     # Update the GeoDataFrame with the new values from the DataFrame
     for column in df.columns:
+        print(F"{column}")
         if column != common_column:
-            merged_gdf[column] = merged_gdf[f'{column}_updated']
-            merged_gdf.drop(columns=[f'{column}_updated'], inplace=True)
+            if f'{column}_updated' in merged_gdf.columns:
+                merged_gdf[column] = merged_gdf[f'{column}_updated']
+                merged_gdf.drop(columns=[f'{column}_updated'], inplace=True)
 
     # Write the updated GeoDataFrame back to a GeoJSON file
+    for c in merged_gdf.columns:
+        try:
+            print(F"{c}: {merged_gdf[c].unique()}")
+        except AttributeError:
+            print(F"{c}: {merged_gdf[c]}")
     merged_gdf.to_file(geojson_path, driver='GeoJSON')
 
 
@@ -51,3 +62,4 @@ if __name__ == "__main__":
     geojson_path = '../data/spatial/Iowa_BLE_Tracking.geojson'
     excel_path = '../data/spatial/Iowa_BLE_Tracking.xlsx'
     common_column = 'HUC8'  # Replace with the actual common column name
+    update_geojson_with_dataframe(geojson_path, excel_path, common_column)
