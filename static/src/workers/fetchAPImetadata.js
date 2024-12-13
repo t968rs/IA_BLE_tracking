c34009b0cced12ab272bb7a9a10bf7b745454d0a
t968rs <kevtg1212@gmail.com>
2024-12-13 11:57:01 -0700

    popup sanity

--- static/src/workers/fetchAPImetadata.js
+++ static/src/workers/fetchAPImetadata.js
@@ -3,14 +3,14 @@ import * as Comlink from '/static/src/comlink.mjs';
 console.log("Worker initialized: Fetching data");
 
 async function fetchSourcesData(jsonUrl) {
-    console.log(`Fetching JSON from ${jsonUrl}`);
+    // console.log(`Fetching JSON from ${jsonUrl}`);
     try {
         const response = await fetch(jsonUrl);
         if (!response.ok) {
             throw new Error(`Failed to fetch JSON: ${response.statusText}`);
         }
         const data = await response.json();
-        console.log("Fetched JSON:", data); // Verify data is received
+        // console.log("Fetched JSON:", data); // Verify data is received
         return data;
     } catch (error) {
         console.error("Error fetching sources data in worker:", error);
